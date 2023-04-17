cccb404b9396d9e9f7f4a4e2d3499bb145e4bb5a
BeeFriedman <70110787+BeeFriedman@users.noreply.github.com>
2023-04-17 01:51:38 -0400

    added static and template assets, and the wallet address for the index page

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, request
+import requests
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    wallet = request.args.get('wallet')
+
+    if wallet == None:
+        wallet = ''
+        
+    return render_template('index.html', wallet = wallet)
