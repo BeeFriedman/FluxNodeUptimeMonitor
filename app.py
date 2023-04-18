64ce4b3dc1113e8925b738b7d4d8f592e9b4f5ed
BeeFriedman <70110787+BeeFriedman@users.noreply.github.com>
2023-04-18 03:35:46 -0400

    added the flux logo and added the request to verify the wallet

--- app.py
+++ app.py
@@ -9,5 +9,15 @@ def index():
 
     if wallet == None:
         wallet = ''
+    else:
+        url = 'https://api.runonflux.io/daemon/validateaddress'
+        params = {'zelcashaddress' : wallet}
+        response = requests.get(url, params=params)
+
+        if response.status_code == 200:
+            # API call was successful
+            data = response.json()
+            valid = data['data']['isvalid']
+            app.logger.debug(valid)      
         
     return render_template('index.html', wallet = wallet)
