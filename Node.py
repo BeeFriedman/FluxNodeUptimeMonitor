bb0b3820ae91caf22706f6c6458de8edbed63572
BeeFriedman <70110787+BeeFriedman@users.noreply.github.com>
2023-04-27 04:07:19 -0400

    fixed some bugs that come from name changes after breaking the code down into classes

--- Node.py
+++ Node.py
@@ -1,8 +1,11 @@
+import sys
 import requests, json
 
 class Node:
-    def __init__(self, ip):
+    def __init__(self, tier, ip, rank):
         self.ip = ip
+        self.tier = tier
+        self.rank = rank
 
     def get_benchmark_results(self):
         if self.ip.find(':') == -1:
@@ -14,3 +17,14 @@ class Node:
         data = response.json()
         inner_data = json.loads(data['data'])
         return inner_data['status']
+    
+    def set_status(self, status):
+        self.status = status
+
+    def get_values(self):
+        return {'tier' : self.tier, 'ip' : self.ip, 'rank' : self.rank, 'status' : self.status}
+
+
+    def get_status(self):
+        return self.status          
+    
--- app.py
+++ app.py
@@ -8,20 +8,25 @@ app = Flask(__name__)
 def index():
     wallet_address = request.args.get('wallet', '')
     pop_up = False
-    node_list = [{'tier': '', 'ip': 'No nodes are running on this wallet address', 'rank': ''}]
+    node_output = [{'tier': '', 'ip': 'No nodes are running on this wallet address', 'rank': ''}]
 
     if wallet_address:
         wallet = Wallet(wallet_address)
         if not wallet.is_valid():
             pop_up = True
         else:
-            node_list = [Node(node['ip']) for node in wallet.get_node_list()]
+            node_output = []
+            node_list = []
+            for node in wallet.get_node_list():
+                node_list.append(Node(node['tier'], node['ip'], node['rank']))
+
             for node in node_list:
                 status = node.get_benchmark_results()
                 if status == 'failed':
                     status = '/static/cancel-icon.svg'
                 else:
                     status = '/static/green-checkmark-line-icon.svg'    
-            node['status'] = status;  
-
-    return render_template('index.html', wallet=wallet_address, pop_up=pop_up, node_list=node_list)
+                node.set_status(status);  
+                node_output.append(node.get_values())
+        app.logger.debug(node_output)
+    return render_template('index.html', wallet=wallet_address, pop_up=pop_up, node_output=node_output)
