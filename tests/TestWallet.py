d4c82cf8451d6a76af4a02df1d8584bb27cf18f6
= <70110787+BeeFriedman@users.noreply.github.com>
2023-04-30 23:28:42 -0400

    fixed minor bug and README

--- tests/TestWallet.py
+++ tests/TestWallet.py
@@ -2,6 +2,8 @@ import unittest
 from Wallet import Wallet
 
 class TestWallet(unittest.TestCase):
+    #tests that a valid wallet returns the right true
+    #and that an invalid wallet returns false.
     def test_is_valid(self):
         wallet = Wallet('t1NGCodd8fdGfbxyENtmHQ4yrVYqQ2Rbf98')
         self.assertTrue(wallet.is_valid())
@@ -9,6 +11,7 @@ class TestWallet(unittest.TestCase):
         wallet = Wallet('invalid_address')
         self.assertFalse(wallet.is_valid())
 
+    #tests that we are getting back a list of data from the api.
     def test_get_node_list(self):
         wallet = Wallet('t1NGCodd8fdGfbxyENtmHQ4yrVYqQ2Rbf98')
         node_list = wallet.get_node_list()
